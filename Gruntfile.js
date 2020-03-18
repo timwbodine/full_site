df3c807a8efdbbc0759ac6c76a718a5c678c0f3c
skrimbleshanks <timwbodine@gmail.com>
2020-03-18 09:55:18 -0400

    add files

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) { 
+  grunt.initConfig({
+    watch: {
+      sass: {
+        files: '**/*.sass',
+        tasks: ['sass'],
+        options: {
+        livereload: 35729
+        }
+      },
+      bake: {
+        files: ["html/**"],
+        tasks: "bake:build"
+      },
+      all: { files: ['**/*.html'], options: { livereload: true } } 
+    },
+    sass: { dev: { files: { 'css/main.css':'sass/main.sass' } } },
+    bake: {
+      build: {
+        files: {
+        "html/index.html" : "html/base.html"
+        }
+      }
+    }
+
+  });
+  grunt.loadNpmTasks( "grunt-bake" );
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
---
+++ readme.md
@@ -0,0 +1,15 @@
+# README 
+
+A simple skeleton for starting sass/javascript/jquery projects.
+Uses Grunt to automate sass compilation and html preprocessing with grunt-bake templates.
+
+
+## Installation and usage
+
+clone to your local machine, cd into project directory and install dependencies
+with ```npm install```.
+Install Grunt command line interface with ```npm install -g grunt-cli``` 
+Run ```grunt watch```from the root folder and then open the index.html with your browser.  Changes should now update dynamically.
+
+
+
