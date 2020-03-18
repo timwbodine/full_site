c78eaa72955077b80fe4aeaa1693c192720d509f
skrimbleshanks <timwbodine@gmail.com>
2020-03-18 14:24:10 -0400

    basic sidebar functionality

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,10 @@
 $(document).ready(() => {
+    $(".main_content").children().css("display","none"); 
+    $(".main_content").children("#home").css("display","block");
+    $(".nav-button").click(function() {
+        let thing = $(this).attr('data');
+        $(".main_content").children().css("display","none");
+        $(".main_content").children('#' + thing).css("display","block");
+        $(".header-title").text(thing);
+    });
 })
